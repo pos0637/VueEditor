d57aecc3ec846a547c042db7dad563ba62c0e6d3
root <root@DESKTOP-MC70ATB.localdomain>
2020-08-14 02:24:50 +0800

    添加动态加载示例。

--- research/designer/src/views/Home.vue
+++ research/designer/src/views/Home.vue
@@ -1,21 +1,24 @@
 <template>
     <div class="home">
         <img alt="Vue logo" src="../assets/logo.png" />
-        <a-button type="primary">
+        <a-button type="primary" @click="foo">
             Add Component
         </a-button>
         <HelloWorld msg="Welcome to Your Vue.js App" />
     </div>
 </template>
 
-<script>
-// @ is an alias to /src
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue';
 
-export default {
-    name: 'Home',
-    components: {
-        HelloWorld
+@Component({
+    components: { HelloWorld }
+})
+export default class Home extends Vue {
+    async foo() {
+        const loader = await import(`@/views/TestComponent.vue`);
+        console.debug(loader);
     }
-};
+}
 </script>
---
+++ research/designer/src/views/TestComponent.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>TestComponent</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class TestComponent extends Vue {
+    foo() {
+        console.debug('TestComponent::foo');
+    }
+}
+</script>
