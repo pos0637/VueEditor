d648c74ff8dbf14bed642a86c74ff14ae1850f2f
root <root@DESKTOP-MC70ATB.localdomain>
2020-08-29 21:55:06 +0800

    添加可拖拽组件。

---
+++ research/designer/src/framework/draggableComponent.vue
@@ -0,0 +1,34 @@
+<template>
+    <div ref="draggable" style="display: inline-block;">{{ name }}</div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import $ from 'jquery';
+import 'jqueryui';
+
+/**
+ * 可拖拽组件
+ */
+@Component
+export default class DraggableComponent extends Vue {
+    /**
+     * 名称
+     */
+    @Prop() private name!: string;
+
+    /**
+     * 路径
+     */
+    @Prop() private path!: string;
+
+    mounted() {
+        $(this.$refs.draggable).draggable({
+            cursor: 'move',
+            helper: 'clone',
+            zIndex: -1,
+            stop: () => this.$framework.attachComponent(this.path)
+        });
+    }
+}
+</script>
--- research/designer/src/views/Home.vue
+++ research/designer/src/views/Home.vue
@@ -1,30 +1,20 @@
 <template>
     <div class="home">
         <img alt="Vue logo" src="../assets/logo.png" />
-        <div ref="draggable" style="display: inline-block;">TestComponent</div>
-        <DivComponent ref="container" style="width: 100%; height: 100px;" />
+        <DraggableComponent name="TestComponent" path="@/views/TestComponent.vue" />
+        <DivComponent style="width: 100%; height: 100px;" />
         <HelloWorld msg="Welcome to Your Vue.js App" />
     </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import $ from 'jquery';
-import 'jqueryui';
+import DraggableComponent from '@/framework/draggableComponent.vue';
 import HelloWorld from '@/components/HelloWorld.vue';
 import DivComponent from '@/components/DivComponent.vue';
 
 @Component({
-    components: { HelloWorld, DivComponent }
+    components: { DraggableComponent, HelloWorld, DivComponent }
 })
-export default class Home extends Vue {
-    mounted() {
-        $(this.$refs.draggable).draggable({
-            cursor: 'move',
-            helper: 'clone',
-            zIndex: -1,
-            stop: () => this.$framework.attachComponent('@/views/TestComponent.vue')
-        });
-    }
-}
+export default class Home extends Vue {}
 </script>
